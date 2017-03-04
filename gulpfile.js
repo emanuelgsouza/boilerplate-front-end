39ba957051c2a1ac87e982daba58bcf29e4a6a5c
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-03-03 23:03:51 -0300

    Insert pug to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
-const gulp = require('gulp');
-const glob = require('glob');
-const br = require('browser-sync');
-const reload = br.reload;
+const gulp = require('gulp')
+const glob = require('glob')
+const br = require('browser-sync')
+const reload = br.reload
 
-const args = require('yargs').default('production', false).alias('p', 'production').argv;
+const args = require('yargs')
+  .default('production', false)
+  .alias('p', 'production').argv
 
 glob.sync('./gulpTasks/*.js', { realPath: true })
   .forEach(function (file) {
-    require(file)(args, reload);
-});
+    require(file)(args, reload)
+})
 
-gulp.task('default', ['sass', 'imagemin', 'htmlmin', 'script'], function () {
+const dependences = ['sass', 'imagemin', 'compilePug','htmlmin', 'script']
+
+gulp.task('default', dependences, function () {
   br.init({
     server: {
       baseDir: './dist',
       index: 'index.html'
     }
-  });
+  })
 
   if (!args.production) {
-    gulp.watch('./src/js/*.js', ['script']);
-    gulp.watch('./src/assets/sass/**/*.sass', ['sass']);
-    gulp.watch('./src/**/*.html', ['htmlmin']);
+    gulp.watch('./src/js/*.js', ['script'])
+    gulp.watch('./src/assets/sass/**/*.sass', ['sass'])
+    gulp.watch('./src/**/*.html', ['htmlmin'])
+    gulp.watch('./src/templates/**/*.pug', ['compilePug'])
   }
-});
+})
