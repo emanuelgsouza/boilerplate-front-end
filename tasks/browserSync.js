c7bcaac127caea5ed98d79e726dca1bdd137bd35
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-07-27 21:21:38 -0300

    Update pug files

--- tasks/browserSync.js
+++ tasks/browserSync.js
@@ -1,13 +1,12 @@
 const br = require('browser-sync')
-const path = require('path')
 
 module.exports = gulp => {
   gulp.task('server', function () {
     const dir = ['./dist/**/**']
     br.init(dir, {
       server: {
-        baseDir: path.resolve(__dirname, '/dist'),
-        index: path.resolve(__dirname, '/dist/index.html')
+        baseDir: './dist',
+        index: 'index.html'
       },
       open: false
     })
